9c441dc516717645b2958168c2598e224774b4aa
Abdul Khan <akhan17@outlook.com>
2020-08-08 19:54:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
     def new
     end
-  
+
     def create
       if params[:name].present?
-        
+
         session[:name] = params[:name]
         redirect_to :root
       else
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
         redirect_to :login
       end
     end
-  
+
     def destroy
       session.delete :name if current_user.present?
       redirect_to :login
